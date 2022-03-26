dd5d6f21d1c1370610edaad99f5643a3985943a1
Vinícius Ventura <vlv2@cin.ufpe.br>
2022-03-26 13:25:11 -0300

    Primeira entrega

--- client.py
+++ client.py
@@ -1,14 +1,35 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 from checksum import checksum
 
-server_name = "localhost"
-server_port = 12000
-message = "Hello from client!"
 
-conn = socket(AF_INET, SOCK_DGRAM)
-conn.sendto(message.encode(), (server_name, server_port))
+def get_package(message: str) -> bytes:
+    msg_to_send = message + "\x01" + checksum([message])
+    return msg_to_send.encode()
 
-msg_received, server_address = conn.recvfrom(2048)
-print(f"Message recieved from {server_address}: {msg_received.decode()}")
 
-conn.close()
+def send_valid_checksum(conn: socket):
+    message = input("Valid checksum - Enter text: ")
+    conn.sendto(get_package(message), (server_name, server_port))
+
+
+def send_not_valid_checksum(conn: socket):
+    message = input("Not valid checksum - Enter text: ")
+    msg_to_send = message + "\x01" + "100011010100101010"
+    conn.sendto(msg_to_send.encode(), (server_name, server_port))
+
+
+if __name__ == "__main__":
+    server_name = "localhost"
+    server_port = 12000
+    conn = socket(AF_INET, SOCK_DGRAM)
+
+    for i in range(2):
+        if i == 0:
+            send_valid_checksum(conn)
+        else:
+            send_not_valid_checksum(conn)
+
+        msg_received, server_address = conn.recvfrom(2048)
+        print(f"Message recieved from {server_address}: {msg_received.decode()}")
+
+    conn.close()
--- server.py
+++ server.py
@@ -1,15 +1,29 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 from checksum import checksum
 
-server_port = 12000
+ERROR_MESSAGE = "Checksum is not equal."
+SUCCESS_MESSAGE = "Checksum is equal."
 
-conn = socket(AF_INET, SOCK_DGRAM)
-conn.bind(("", server_port))
-message = "Hello from Server!"
 
-print(f"Server is listening on port: {server_port}")
+def parse_package(message: bytes) -> tuple:
+    return message.decode().split("\x01")
 
-while True:
-    client_message, client_address = conn.recvfrom(2048)
-    print(f"Message recieved from {client_address}: {client_message.decode()}")
-    conn.sendto(message.encode(), client_address)
+
+if __name__ == "__main__":
+    server_port = 12000
+    conn = socket(AF_INET, SOCK_DGRAM)
+    conn.bind(("", server_port))
+
+    print(f"Server is listening on port: {server_port}")
+    while True:
+        package, client_address = conn.recvfrom(2048)
+        client_message, message_checksum = parse_package(package)
+        print(f"Message recieved from {client_address}")
+
+        msg_to_send = ""
+        if message_checksum == checksum([client_message]):
+            msg_to_send = SUCCESS_MESSAGE
+        else:
+            msg_to_send = ERROR_MESSAGE
+
+        conn.sendto(msg_to_send.encode(), client_address)
