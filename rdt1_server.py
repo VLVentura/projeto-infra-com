eb5a9e9a40bf713f70afdb6cc8448c89d7a9c76b
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-17 20:28:31 -0300

    ADD to rdt1_server.py (function) deliver_data

--- rdt1_server.py
+++ rdt1_server.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 import udp_imports as udp
+def deliver_data(data :str, client_add):
+    print(f"This was yous message from {client_add}: {data}")
+    udp.CONN.sendto(data.encode(),client_add)
+    return
 if __name__ == "__main__":
     rdt_server_loop()
