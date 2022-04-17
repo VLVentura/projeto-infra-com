6ddf259238b07bb802d70c97558494dda51ff715
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-17 20:39:56 -0300

    REFACTOR rdt1_server.py remove return statements

--- rdt1_server.py
+++ rdt1_server.py
@@ -13,7 +13,6 @@ def rdt_server_loop():
 
 def rdt_rcv(pkt :bytes,client_add):
     deliver_data(extract(pkt),client_add)
-    return
 
 def extract(pkt :bytes)->str:
     return pkt.decode()
@@ -21,7 +20,6 @@ def extract(pkt :bytes)->str:
 def deliver_data(data :str, client_add):
     print(f"This was yous message from {client_add}: {data}")
     udp.CONN.sendto(data.encode(),client_add)
-    return
 
 if __name__ == "__main__":
     rdt_server_loop()
