24e2e24b3377f1d9b75bf9285f29c490d6822d07
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-17 20:34:14 -0300

    REFACTOR rdt1_client.py

--- rdt1_server.py
+++ rdt1_server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import udp_imports as udp
+
+# State: wait for call from below
 def rdt_server_loop():
     udp.CONN.bind(("",udp.SERVER_PORT))
     print(f"Server is listening on port: {udp.SERVER_PORT}")
@@ -7,14 +9,19 @@ def rdt_server_loop():
         pkt, client_address = udp.CONN.recvfrom(2048)
         print(f"Connection stablished with {client_address}")
         rdt_rcv(pkt,client_address);
+
+
 def rdt_rcv(pkt :bytes,client_add):
     deliver_data(extract(pkt),client_add)
     return
+
 def extract(pkt :bytes)->str:
     return pkt.decode()
+
 def deliver_data(data :str, client_add):
     print(f"This was yous message from {client_add}: {data}")
     udp.CONN.sendto(data.encode(),client_add)
     return
+
 if __name__ == "__main__":
     rdt_server_loop()
