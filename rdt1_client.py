609f1c7c62528e325f3c56ad864365036ba93601
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-17 20:31:44 -0300

    ADD to rdt1_client.py (function) make_pkt

--- rdt1_client.py
+++ rdt1_client.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import udp_imports as udp
+def make_pkt(pkt :udp.Packet,data :bytes)->udp.Packet:
+    pkt.data = data
+    return pkt
 def udt_send(pkt :udp.Packet):
     udp.CONN.sendto(pkt.data, (udp.SERVER_NAME,udp.SERVER_PORT))
     return
