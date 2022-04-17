7f2b586ff7e4001d6e52484f2f7d400cabb6f692
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-17 20:29:16 -0300

    ADD to rdt1_server.py (function) rdt_rcv

--- rdt1_server.py
+++ rdt1_server.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import udp_imports as udp
+def rdt_rcv(pkt :bytes,client_add):
+    deliver_data(extract(pkt),client_add)
+    return
 def extract(pkt :bytes)->str:
     return pkt.decode()
 def deliver_data(data :str, client_add):
