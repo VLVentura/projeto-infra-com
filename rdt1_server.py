35ec571cbc367692a43bdd8d414e096f797580fd
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-17 20:29:35 -0300

    ADD to rdt1_server.py (function) rdt_server_loop

--- rdt1_server.py
+++ rdt1_server.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 import udp_imports as udp
+def rdt_server_loop():
+    udp.CONN.bind(("",udp.SERVER_PORT))
+    print(f"Server is listening on port: {udp.SERVER_PORT}")
+    while True:
+        pkt, client_address = udp.CONN.recvfrom(2048)
+        print(f"Connection stablished with {client_address}")
+        rdt_rcv(pkt,client_address);
 def rdt_rcv(pkt :bytes,client_add):
     deliver_data(extract(pkt),client_add)
     return
