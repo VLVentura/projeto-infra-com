42fcda8ed8a1ecb7377e639a08eaaa271006f3c5
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-17 20:28:53 -0300

    ADD to rdt1_server.py (function) extract

--- rdt1_server.py
+++ rdt1_server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import udp_imports as udp
+def extract(pkt :bytes)->str:
+    return pkt.decode()
 def deliver_data(data :str, client_add):
     print(f"This was yous message from {client_add}: {data}")
     udp.CONN.sendto(data.encode(),client_add)
