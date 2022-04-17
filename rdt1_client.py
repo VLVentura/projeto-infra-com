e4187c23546b720e333d46aed992cde0aa12af2f
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-17 20:31:29 -0300

    ADD to rdt1_client.py (function) udt_send

--- rdt1_client.py
+++ rdt1_client.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 import udp_imports as udp
+def udt_send(pkt :udp.Packet):
+    udp.CONN.sendto(pkt.data, (udp.SERVER_NAME,udp.SERVER_PORT))
+    return
 if __name__ == "__main__":
     rdt_send(input("MESSAGE: "))
