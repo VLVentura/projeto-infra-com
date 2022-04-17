1e00b9a82c390de68f80d6e158c911e91a396b12
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-17 20:30:28 -0300

    ADD (file) rdt1_client.py

--- rdt1_client.py
+++ rdt1_client.py
@@ -1,14 +1,1 @@
 #!/usr/bin/env python3
-import client, packet
-
-def rdt_send(data :bytes):
-        #TODO implement rdt_send
-    return ;
-
-def make_pkt(packet :packet.Packet,data :bytes):
-    #TODO implement make_pkt
-    return ;
-
-def udt_send(packet :packet.Packet):
-    #TODO implement udt_send
-    return;
