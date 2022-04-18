4cd78e4db420b816b25fd3a990a8c50970114370
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-18 09:59:13 -0300

    ADD to udp_imports.py (imports) from server and rdt2_client and rdt2_server add parse_package, import * and change SERVER_NAME to SERVER_HOST

--- udp_imports.py
+++ udp_imports.py
@@ -2,8 +2,12 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 from checksum import checksum
 from client import get_package
+from server import parse_package
 from packet import Packet
+from rdt2_client import *
+from rdt2_server import *
 
 SERVER_PORT = 12000
-SERVER_NAME = "localhost"
+CLIENT_PORT = 5600
+SERVER_HOST = "localhost"
 CONN = socket(AF_INET, SOCK_DGRAM)
