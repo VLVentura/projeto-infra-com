fd1972e9e95691f946f09c091b4584bb66392820
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-18 18:24:20 -0300

    REFACTOR rdt21_client.py newlines and comments removal

--- rdt21_client.py
+++ rdt21_client.py
@@ -18,20 +18,22 @@ def isNACK(rcvpkt_data :bytes) -> bool:
 def isACK(rcvpkt_data :bytes) -> bool:
     return udp.parse_package(rcvpkt_data) == "1"
 
-# State wait for call from above
 def compute_chksum(pkt :udp.Packet) -> str:
     data_to_checksum = compose(pkt)
     return udp.checksum([data_to_checksum])
+
 def wait_for_acknoewldgemnt(rcvpkt: bytes, pkt :udp.Packet):
     while (udp.dataIntegrity(rcvpkt) or isNACK(rcvpkt)):
         print("INVALID MESSAGE RECEIVED")
         udt_send(make_pkt(pkt,pkt.checksum))
+
 def wait_for_call(data :str, seq :int)->udp.Packet:
     pkt = udp.Packet(data, 4,udp.CLIENT_PORT,udp.SERVER_PORT,seq, "")
     chksum = compute_chksum(pkt)
     sndpkt = make_pkt(pkt, chksum)
     udt_send(sndpkt)
     return sndpkt
+
 def rdt_send(data :str):
     #State wait for call0 from above
     sndpkt0 = wait_for_call(data, 0)
