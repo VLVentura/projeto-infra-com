5cfcff74b2dfa23aab5edb54587bebabe0324ef6
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-18 18:21:40 -0300

    REFACTOR packet.py

--- packet.py
+++ packet.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 class Packet:
-    def __init__(self, data, src_port, dest_port, checksum):
+    def __init__(self, data,lenght, src_port, dest_port, seq,checksum):
         self.data = data
+        self.seq = seq
+        self.length = lenght
         self.src_port = src_port
         self.dest_port = dest_port
         self.checksum = checksum
+
