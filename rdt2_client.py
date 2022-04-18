bb12f2c9535f852f18ac8901d0d29cb7d39455a8
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-18 10:08:06 -0300

    REFACTOR rdt2_client.py add newlinets

--- rdt2_client.py
+++ rdt2_client.py
@@ -4,13 +4,16 @@ import udp_imports as udp
 def make_pkt(sndpkt :udp.Packet,data :str, checksum :str)->udp.Packet:
     sndpkt.data = (data + "\x01" + checksum).encode()
     return sndpkt
+
 def udt_send(sndpkt :udp.Packet):
     udp.CONN.sendto(sndpkt.data, (udp.SERVER_HOST,sndpkt.dest_port))
+
 def isNACK(rcvpkt_data :bytes) -> bool:
     return udp.parse_package(rcvpkt_data) == "0"
 
 def isACK(rcvpkt_data :bytes) -> bool:
     return udp.parse_package(rcvpkt_data) == "1"
+
 # State wait for call from above
 def rdt_send(data :str):
     newpkt = udp.Packet("null", udp.CLIENT_PORT,udp.SERVER_PORT, "00000000")
