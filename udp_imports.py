6b79ad01bd652c39eef27f4ac1809d7656e0072a
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-18 18:20:31 -0300

    ADD to udp_imports.py (imports) rdt21_client and rdt21_server

--- udp_imports.py
+++ udp_imports.py
@@ -4,8 +4,8 @@ from checksum import checksum
 from client import get_package
 from server import parse_package
 from packet import Packet
-from rdt2_client import *
-from rdt2_server import *
+from rdt21_client import *
+from rdt21_sever import *
 
 SERVER_PORT = 12000
 CLIENT_PORT = 5600
