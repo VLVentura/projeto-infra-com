aa8de606c5e24be51b75a6556588be062a2c1c9a
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-22 20:14:44 -0300

    ADD (partial implementation) calc_checksum, str_list2bin, sum_bytes, verify_bytes

--- checksum.py
+++ checksum.py
@@ -2,15 +2,24 @@
 from packet import Packet
 
 class Checksum:
-        def __init__(self, segment, target=bytes(32)):
+        def __init__(self, segment :Packet, target=bytearray(15)):
             self.segment = segment
             self.target = target
+            self.binArr = []
+
+        def calc_checksum(self)->bytes:
+            Checksum.str_list2bin(self)
+            return Checksum.sum_bytes(self)
 
         def str_list2bin(self):
+            map(lambda x: bytearray(x, 'utf-8'), self.segment.segment) #HACK this is very hacky
             pass
 
-        def sum_bytes(self):
-            pass
+        def sum_bytes(self)->bytes:
+            sum_bytes = self.segment.segment[0];
+            [sum_bytes := bytearray(sum_bytes + x) for x in self.segment.segment[1:]]
+            return sum_bytes
 
-        def verify_bytes(self, target):
+        @staticmethod
+        def verify_bytes(target):
             pass
