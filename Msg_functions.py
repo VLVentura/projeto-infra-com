696dabc95f4a972cd05ad36974294aa9330fd83d
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-22 14:44:22 -0300

    ADD (comentários) para cut_sg, unpack_helper, has_seq e is_data

--- Msg_functions.py
+++ Msg_functions.py
@@ -10,6 +10,7 @@ def __udt_recv(sockd, length):  #socket com a msg e tamanho max da msg
     return rmsg                 #Retorna byte de msg recebido
 
 def __cut_msg(byte_msg):        #Input: mensagem no formato de byte
+    """ Split segmento em Payload e dados"""
     global PAYLOAD
     if len(byte_msg) > PAYLOAD:
         msg = byte_msg[0:PAYLOAD]
@@ -32,17 +33,20 @@ def __make_data(seq_num, data):
     complete_msg = msg_format.pack(TYPE_DATA, seq_num, checksum, socket.htons(len(data))) + data #hton = host to network byte order
     return complete_msg #Msg com o header + data
 
-def __unpack_helper(msg):   #"destrincha" a msg recebida
+def __unpack_helper(msg):
+    """ Destrincha a menssagem recebida"""
     global MSG_FORMAT
     size = struct.calcsize(MSG_FORMAT)
     (msg_type, seq_num, recv_checksum, payload_len), payload = struct.unpack(MSG_FORMAT, msg[:size]), msg[size:] #unpack descompacta de acordo com o formato, precisa ter tamanho equivalente ao do calczise
     return (msg_type, seq_num, recv_checksum, socket.ntohs(payload_len)), payload  # ntohs = network to host byte order
 
-def __has_seq(recv_msg, seq_num):   #Checa se o pacote recebido contem o sequence equivalente ao sequence real
+def __has_seq(recv_msg, seq_num):
+    """ Checa se pacote recebido contem o sequence equivalente ao sequence esperado"""
     (msg_type, recv_seq_num, _, _), _ = __unpack_helper(recv_msg)
     return recv_seq_num == seq_num
 
 def __is_data(recv_pkt, seq_num):   #Checa se a msg é dado ou não
+    " Checa se a msg é dado ou não"
     global TYPE_DATA
     (pkt_type, pkt_seq, _, _), _ = __unpack_helper(recv_pkt)
     return pkt_type == TYPE_DATA and pkt_seq == seq_num #true or false
