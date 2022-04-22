725f88fbcc77d7872569b8b7021194da3de14c07
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-22 09:58:43 -0300

    ADD (concrete class) Manipulation

---
+++ manipulation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from rdt import rdt
+
+
+class Manipulation(rdt):
+    def __init__(self) -> None:
+        pass
+
+    def extract(self, rcvpkt :bytes, data :str):
+        return
+
+    def deliver_data(self, data :str):
+        return
