74075462bd13a0433d6a3f8c286d895daec63902
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-22 12:37:44 -0300

    ADD (method) send, udt_send, rcv, make_socket to transfer.py

--- transfer.py
+++ transfer.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 from rdt import rdt
-
+from packet import Packet
+from socket import socket, AF_INET, SOCK_DGRAM
 
 class Transfer(rdt):
-    def __init__(self):
+    def make_socket(self):
+        return socket(AF_INET, SOCK_DGRAM)
+
+    def __init__(self, data, src_port, dest_port,server_name="localhost",length=32,seg_num=0 ,checksum=00000000000):
+        self.data = data
+        self.connection = Transfer.make_socket(self)
+        self.segment = Packet()
+        self.server_name = server_name
+        self.segment.set_header(length, seg_num, src_port, dest_port, checksum)
         return
 
-    def send(self,data: str):
+    def send(self,data):
+        #compute checksum here
+        self.segment.make_pkt(self.segment.segnum, data, checksum)
+        Transfer.udt_send(self, self.segment)
         return
 
-    def udt_send(self, sndpkt : bytes):
-        return
+    def udt_send(self, sndpkt:str):
+        self.connection.sendto(sndpkt.encode(), (self.server_name, self.segment.dest_port))
 
     def rcv(self, rcvpkt :bytes):
-        return
+        return self.connection.recvfrom(2048)
+        pass
