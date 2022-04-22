d315ed52643b630bef7a320bd240f327fd79efb0
LuanCavalcanti07 <89256475+LuanCavalcanti07@users.noreply.github.com>
2022-04-22 14:09:35 -0300

    Update integrity.py

--- integrity.py
+++ integrity.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from rdt import rdt
-
+import struct
+import socket
+from Msg_functions import Msg_functions
 
 class Integrity(rdt):
     def __init__(self) -> None:
@@ -12,7 +14,8 @@ class Integrity(rdt):
         return
 
     def isACK0(self, rcvpkt :bytes, seqNum :int):
-        return
+        (msg_type, recv_seq_num, _, _), _ = __unpack_helper(recvpkt)
+        return  msg_type == 11 and recv_seq_num == seqNum       #11 é valor arbitrário, escolhido na criação do ack, representando o tipo dele
 
     def isACK1(self, rcvpkt :bytes, segNum :int):
         return
@@ -24,13 +27,17 @@ class Integrity(rdt):
         return
 
     def notcorrupt(self ,rcvpkt :bytes):
-        return
+        (msg_type, seq_num, recv_checksum, payload_len), payload = __unpack_helper(recvpkt)
+        init_msg = struct.Struct('B?HH').pack(msg_type, seq_num, 0, socket.htons(payload_len)) + payload
+        calc_checksum = checksum(bytearray(init_msg)) #precisa chamar corretamente, tá errado isso aq
+        return  recv_checksum == calc_checksum  #true se não corrompido
 
     def stop_timer(self):
         return
 
-    def has_seq0(self, rcvpkt :bytes):
-        return
+   def has_seq0(self, rcvpkt :bytes, seqNum :int):
+        (msg_type, recv_seq_num, _, _), _ = __unpack_helper(rcvpkt)
+        return  recv_seq_num == seqNum  #true se recebe pacote do sequence number
 
     def has_seq1(self, rcvpkt :bytes):
         return
