0710cfc0c5629457fbdd6f863c13a6dbddcca23b
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-22 12:38:58 -0300

    ADD (method) extract to manipulation.py

--- manipulation.py
+++ manipulation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 from rdt import rdt
 
-
 class Manipulation(rdt):
-    def __init__(self) -> None:
+    def __init__(self, rcvpkt) -> None:
+        self.rcvpkt = rcvpkt
         pass
 
     def extract(self, rcvpkt :bytes, data :str):
-        return
+        return self.rcvpkt.decode().split("\x01")
 
     def deliver_data(self, data :str):
-        return
+        pass
+
