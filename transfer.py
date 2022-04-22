ef2c5a265557d904ad0a63442afe7a755abccfd8
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-22 09:58:28 -0300

    ADD (concrete class) Transfer

---
+++ transfer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from rdt import rdt
+
+
+class Transfer(rdt):
+    def __init__(self):
+        return
+
+    def send(self,data: str):
+        return
+
+    def udt_send(self, sndpkt : bytes):
+        return
+
+    def rcv(self, rcvpkt :bytes):
+        return
