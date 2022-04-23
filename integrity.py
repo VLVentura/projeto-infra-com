f50d7882924ced6e938fcad0cf502d2396d368eb
HACardoso <89423037+HACardoso@users.noreply.github.com>
2022-04-22 21:34:39 -0300

    Update integrity.py
    MInimal Changes

--- integrity.py
+++ integrity.py
@@ -3,8 +3,9 @@
 from rdt import rdt
 import struct
 import socket
-from Msg_functions import Msg_functions
-
+import Msg_functions
+from checksum import Checksum
+   
 class Integrity(rdt):
     def __init__(self) -> None:
         pass
@@ -14,7 +15,7 @@ class Integrity(rdt):
         return
 
     def isACK0(self, rcvpkt :bytes, seqNum :int):
-        (msg_type, recv_seq_num, _, _), _ = __unpack_helper(recvpkt)
+        (msg_type, recv_seq_num, _, _), _ = Msg_functions.__unpack_helper(rcvpkt)
         return  msg_type == 11 and recv_seq_num == seqNum       #11 é valor arbitrário, escolhido na criação do ack, representando o tipo dele
 
     def isACK1(self, rcvpkt :bytes, segNum :int):
@@ -27,16 +28,16 @@ class Integrity(rdt):
         return
 
     def notcorrupt(self ,rcvpkt :bytes):
-        (msg_type, seq_num, recv_checksum, payload_len), payload = __unpack_helper(recvpkt)
+        (msg_type, seq_num, recv_checksum, payload_len), payload = Msg_functions.__unpack_helper(rcvpkt)
         init_msg = struct.Struct('B?HH').pack(msg_type, seq_num, 0, socket.htons(payload_len)) + payload
-        calc_checksum = checksum(bytearray(init_msg)) #precisa chamar corretamente, tá errado isso aq
+        calc_checksum = Checksum.calc_checksum(bytearray(init_msg)) #precisa chamar corretamente, tá errado isso aq
         return  recv_checksum == calc_checksum  #true se não corrompido
 
     def stop_timer(self):
         return
 
-   def has_seq0(self, rcvpkt :bytes, seqNum :int):
-        (msg_type, recv_seq_num, _, _), _ = __unpack_helper(rcvpkt)
+    def has_seq0(self, rcvpkt :bytes, seqNum :int):
+        (msg_type, recv_seq_num, _, _), _ = Msg_functions.__unpack_helper(rcvpkt)
         return  recv_seq_num == seqNum  #true se recebe pacote do sequence number
 
     def has_seq1(self, rcvpkt :bytes):
