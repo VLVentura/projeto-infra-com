ea41f655a2df1df11dec5ca0cc9f12793a719977
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-22 22:49:41 -0300

    REFACOTR (less-lines) all methods

--- integrity.py
+++ integrity.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from manipulation import Manipulation
 from rdt import rdt
 import struct
 import socket
@@ -11,34 +12,23 @@ class Integrity(rdt):
         pass
 
     def corrupt(self, rcvpkt :bytes):
-
-        return
+        return Checksum.sum_bytes(rcvpkt) != rcvpkt[0:15] # position of chekcsum in the segment
 
     def isACK0(self, rcvpkt :bytes, seqNum :int):
-        (msg_type, recv_seq_num, _, _), _ = Msg_functions.__unpack_helper(rcvpkt)
-        return  msg_type == 11 and recv_seq_num == seqNum       #11 é valor arbitrário, escolhido na criação do ack, representando o tipo dele
+        extracted_data = Manipulation.extract(rcvpkt)
+        return extracted_data[0:-2] == 'ACK'
 
     def isACK1(self, rcvpkt :bytes, segNum :int):
-        return
-
-    def timeout(self):
-        return
-
-    def start_timer(self):
-        return
+        extracted_data = Manipulation.extract(rcvpkt) # return data and seqnum
+        return extracted_data[0:-2] == 'ACK'
 
     def notcorrupt(self ,rcvpkt :bytes):
-        (msg_type, seq_num, recv_checksum, payload_len), payload = Msg_functions.__unpack_helper(rcvpkt)
-        init_msg = struct.Struct('B?HH').pack(msg_type, seq_num, 0, socket.htons(payload_len)) + payload
-        calc_checksum = Checksum.calc_checksum(bytearray(init_msg)) #precisa chamar corretamente, tá errado isso aq
-        return  recv_checksum == calc_checksum  #true se não corrompido
+        return Checksum.sum_bytes(rcvpkt) == rcvpkt[0:15]
 
-    def stop_timer(self):
-        return
-
-    def has_seq0(self, rcvpkt :bytes, seqNum :int):
-        (msg_type, recv_seq_num, _, _), _ = Msg_functions.__unpack_helper(rcvpkt)
-        return  recv_seq_num == seqNum  #true se recebe pacote do sequence number
+    def has_seq0(self, rcvpkt :bytes):
+        extracted_data = Manipulation.extract(rcvpkt)
+        return extracted_data[-1] == 0
 
     def has_seq1(self, rcvpkt :bytes):
-        return
+        extracted_data = Manipulation.extract(rcvpkt)
+        return extracted_data[-1] == 1
