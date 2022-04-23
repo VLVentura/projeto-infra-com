1c2087c5c821daa951eba0b91a10bd56f66f386c
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-23 00:47:27 -0300

    ADD (function) server_wait_for_0_from_below

--- server.py
+++ server.py
@@ -4,3 +4,22 @@ from integrity import Integrity
 from transfer import Transfer
 SERVER_PORT = 12000
 SERVER_NAME = "localhost"
+def server_wait_for_0_from_below(server):
+    rcvpkt, address = Transfer.rcv(server)
+    newIntegrityCheck = Integrity()
+    newManipulation = Manipulation(rcvpkt)
+    while True:
+        if(Integrity.corrupt(newIntegrityCheck,rcvpkt) and
+        Integrity.has_seq1(newIntegrityCheck,rcvpkt)):
+            Transfer.set_segnum(server, 1)
+            Transfer.send(server,"ACK")
+            Transfer.set_add(server, address)
+            rcvpkt, address = Transfer.rcv(server)
+
+        elif(Integrity.notcorrupt(newIntegrityCheck,rcvpkt) and
+            Integrity.has_seq0(newIntegrityCheck,rcvpkt)):
+            newManipulation.extract()
+            newManipulation.deliver_data()
+            Transfer.set_segnum(server, 0)
+            Transfer.send(server, "ACK")
+            server_wait_for_1_from_below(server)
