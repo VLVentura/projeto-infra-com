41b4fe7f5522fa792a2b5dffb945c93296a78751
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-22 21:32:32 -0300

    ADD (method implementation) all but verify_bytes

--- checksum.py
+++ checksum.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from packet import Packet
+import copy
 
 class Checksum:
         def __init__(self, segment :Packet, target=bytearray(15)):
@@ -7,19 +8,29 @@ class Checksum:
             self.target = target
             self.binArr = []
 
-        def calc_checksum(self)->bytes:
+        def calc_checksum(self):
             Checksum.str_list2bin(self)
-            return Checksum.sum_bytes(self)
+            map(lambda x: bytearray(x + b'1'), self.segment.segment)
+            return self.segment.segment
 
         def str_list2bin(self):
-            map(lambda x: bytearray(x, 'utf-8'), self.segment.segment) #HACK this is very hacky
+            new_segment = copy.deepcopy(self.segment)
+            map(lambda x: bytearray(x, 'utf-8'), new_segment.segment) #HACK this is very hacky
+            self.segment = new_segment
             pass
-
-        def sum_bytes(self)->bytes:
-            sum_bytes = self.segment.segment[0];
-            [sum_bytes := bytearray(sum_bytes + x) for x in self.segment.segment[1:]]
-            return sum_bytes
 
         @staticmethod
-        def verify_bytes(target):
-            pass
+        def overflow(actsum, arr)->bytearray:
+                if bytearray(actsum, arr)[16] == 1:
+                        return bytearray(b'000000000000001')
+                else:
+                        return bytearray(b'000000000000000')
+
+        def sum_bytes(self)->bytearray:
+            actual_sum = self.segment;
+            [actual_sum := Checksum.overflow(actual_sum, x) + bytearray(actual_sum + x )for x in self.segment.segment[1:]]
+            return actual_sum
+
+        @staticmethod
+        def verify_bytes(pkt,target)->bool:
+                pkt
