3cf71fa065f90a751f11ee215bdff2b540e05752
Vinícius Ventura <vlv2@cin.ufpe.br>
2022-05-05 19:45:24 -0300

    refact(server.py): Simple changes to server.py file

--- server.py
+++ server.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-from socket import socket, AF_INET, SOCK_DGRAM
-=======
 #!/usr/bin/env python3
->>>>>>> rdt3/server
 from rdt import Rdt
 
 
@@ -17,6 +13,7 @@ class Server:
         while True:
             data, client_address = self.__conn.recv()
             print(f"Msg received from: {client_address}: {data}")
+
 
 if __name__ == "__main__":
     server = Server("localhost", 12000)
