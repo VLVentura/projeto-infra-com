953959745e97341353477ceeb5d3b24dceb030fe
HACardoso <89423037+HACardoso@users.noreply.github.com>
2022-05-07 15:18:16 -0300

    Add RDT client

--- client.py
+++ client.py
@@ -1,19 +1,24 @@
-#!/usr/bin/env python3
-from time import sleep
+import socket
 from rdt import Rdt
 
-
-class Client:
-    def __init__(self, server_addr: str, server_port: int):
-        self.__server_info = (server_addr, server_port)
-        self.__conn = Rdt.create_client_connection(server_addr, server_port)
-
-    def run(self):
-        while True:
-            self.__conn.send("Hello World!")
-            sleep(1)
+HOST = "localhost"
+PORT = 12000
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.connect((HOST, PORT))
+conn = Rdt.create_client_connection(HOST, PORT)
 
 
-if __name__ == "__main__":
-    client = Client("localhost", 12000)
-    client.run()
+message = input('Digite o comando\n')
+#s.sendall(str.encode(message))
+conn.send(message)
+while True:
+    if message == 'fechar':
+        break
+    #answer = s.recv(1024)
+    #answer = conn.recv()
+    answer, ender = conn.recv()
+    print(answer)
+    message = input()
+    conn.send(message, ender)
+
+
